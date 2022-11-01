8440d4ceb704235fcc439e67364a16ee32ab2fea
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-11-01 15:47:49 +0100

    iteration2

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.use('/', index)
 const auth = require('./routes/auth')
 app.use('/auth', auth)
 
+const profile = require('./routes/profile')
+app.use('/profile', profile)
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app)
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,26 @@
 const router = require("express").Router();
-const User = require('../models/User.model.js')
+const User = require('../models/User.model.js');
+const bcrypt = require('bcryptjs')
+
 
 /* GET signup page */
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
+router.get("/profile", (req, res, next) => {
+    res.render("profile");
+  });
+
 router.post("/signup", async (req, res) => {
+    const salt = bcrypt.genSaltSync(11)
+    const passwordHash = bcrypt.hashSync(req.body.password, salt)
     await User.create({
         username : req.body.username,
-        password : req.body.password
+        password : passwordHash
     }) 
     res.redirect("/profile");
   });
 
 module.exports = router;
+//
